7ae2813724a5ae47ad83f5dfc10c064ddf6020e1
Turner Systems <248963151+wenochturner-code@users.noreply.github.com>
2025-12-18 13:42:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -54,11 +54,12 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://friendlyticker-frontend(-.*)?\.vercel\.app",
+    allow_origins=["https://friendlyticker-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 
